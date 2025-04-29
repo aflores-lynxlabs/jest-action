63db0d36c0dae63ec93d069c9ed4ffffcf6ed183
Angel Emilio Flores Bonilla <angel.flores@lynx-labs.com>
2025-04-29 14:44:09 -0600

    fix lint

--- src/tests/AcceptCookies.test.tsx
+++ src/tests/AcceptCookies.test.tsx
@@ -5,18 +5,18 @@ describe('AcceptCookies component', () => {
 
     let checkBox: HTMLElement;
     let article: HTMLElement;
-    beforeEach(() => {
-        render(<AcceptCookies />);
-        checkBox = screen.getByLabelText('Acepta las cookies');
-        article = screen.getByRole('article', { name: 'confimación cookies'});
-    });
 
     test('si el check box esta en el componente y si esta desactivado', () => {
+        render(<AcceptCookies />);
+        checkBox = screen.getByLabelText('Acepta las cookies');
         expect(checkBox).toBeInTheDocument();
         expect(checkBox).not.toBeChecked();
     });
 
     test('si cambia el texto al pulsar el check box', () => {
+        render(<AcceptCookies />);
+        checkBox = screen.getByLabelText('Acepta las cookies');
+        article = screen.getByRole('article', { name: 'confimación cookies'});
         fireEvent.click(checkBox);
         expect(checkBox).toBeChecked();
         expect(article.textContent).toBe('Cookies aceptadas')
--- src/tests/TexBox.test.tsx
+++ src/tests/TexBox.test.tsx
@@ -4,23 +4,25 @@ import TextBox from "../components/TexBox";
 describe('TextBox Component', () => {
     let article: HTMLElement;
     let button: HTMLButtonElement;
-    beforeEach(() => {
-        render(<TextBox />);
-        article = screen.getByRole('article', { name: 'párrafo principal'});
-        button = screen.getByRole('button', { name: 'Pulsa para modificar' });
-    })
 
     test('la caja de texto se encuentra en el documento', () => {
+        render(<TextBox />);
+        article = screen.getByRole('article', { name: 'párrafo principal'});
         expect(article).toBeInTheDocument();
     });
 
     test('la caja con el texto tiene un color inicial', () => {
+        render(<TextBox />);
+        article = screen.getByRole('article', { name: 'párrafo principal'});
         expect(article).toHaveStyle({
             backgroundColor: 'indigo'
         });
     });
 
     test('si al pulsar el botón cambia el color de fondo', () => {
+        render(<TextBox />);
+        article = screen.getByRole('article', { name: 'párrafo principal'});
+        button = screen.getByRole('button', { name: 'Pulsa para modificar' });
         fireEvent.click(button);
         expect(article).toHaveStyle({
             backgroundColor: 'tomato'
